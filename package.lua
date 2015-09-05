485b422a21ffd344ed0ad85c98723db6dec93519
james2doyle <james2doyle@gmail.com>
2015-09-05 13:11:09 -0700

    improve package.lua. use init.lua convention

--- package.lua
+++ package.lua
@@ -1,8 +1,24 @@
 return {
   name = "james2doyle/slugify",
-  version = "0.0.1",
+  version = "0.0.2",
   homepage = "https://github.com/james2doyle/lit-slugify",
+  description = "Generate URL-safe slugs",
   dependencies = {
     "james2doyle/string-trim@0.0.1"
+  },
+  tags = {
+    "slugify",
+    "slug",
+    "url"
+  },
+  license = "MIT",
+  author = {
+    name = "James Doyle",
+    email = "james2doyle@gmail.com"
+  },
+  files = {
+    "*.lua",
+    "!tests",
+    "!examples"
   }
 }
